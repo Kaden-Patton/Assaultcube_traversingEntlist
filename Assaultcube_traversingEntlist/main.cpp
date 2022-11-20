d1963fdf9f9299bac127143dc50834014e516828
Kaden <39076173+Kaden-Patton@users.noreply.github.com>
2022-11-19 22:14:04 -0700

    Add project files.

---
+++ Assaultcube_traversingEntlist/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "Memory.h"
+
+struct offsets {
+	uintptr_t entitylist = 0x10f4f8;
+	uintptr_t localPlayer = 0x10f4f4;
+
+	uintptr_t health = 0xF8;
+	uintptr_t name = 0x225;
+} offsets;
+
+typedef char ac_name[16];
+
+int main()
+{
+	Memory mem("ac_client.exe");
+
+	uintptr_t moduleBase = mem.GetModuleAddress("ac_client.exe");
+
+	while (true) {
+		if (GetAsyncKeyState(VK_END)) {
+			break;
+		}
+
+		for (int i = 1; i < 32; i++) {
+			auto entityList = mem.Read<uintptr_t>(moduleBase + offsets.entitylist);
+			auto curEnt = mem.Read<uintptr_t>(entityList + (uintptr_t)(i * 0x4));
+			if (curEnt != 0x00000000) {
+				auto health = mem.Read<int>(curEnt + offsets.health);
+				if (health != 0x00000000)
+				{
+					char* name = mem.ReadString(curEnt + offsets.name);
+					printf("%s ", name);
+					std::cout << std::dec << health << " at index " << std::hex << (uintptr_t)(i * 0x4) << "\n";
+					Sleep(500);
+				}
+			}
+		}
+		Sleep(1);
+	}
+
+	return 0;
+}
